b713db57d06dc7fc793a5c0bcb33be6c941c76dd
yunji <yunjigo92@gmail.com>
2023-02-02 16:07:37 +0900

    feat:  assets, image
    - #2.3 Application Part Three

--- App.js
+++ App.js
@@ -1,19 +1,26 @@
 import AppLoading from 'expo-app-loading';
 import React,{useState} from 'react';
-import { Text } from 'react-native';
+import { Text,Image } from 'react-native';
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+import { Asset,useAssets } from 'expo-asset';
+
+const loadFonts = (fonts) => fonts.map(font => Font.loadAsync(font));
+
+const loadImages = (images) => images.map((image) => {
+  if(typeof image === "string"){
+    return Image.prefetch(image);
+  }else{
+    return Asset.loadAsync(image);
+  }
+})
 
 export default function App() {
-  const [ready, setReady] = useState(false);
-  const onFinish = () => setReady(true);
-  const startLoading = async() => {
-  };
-
-
-  if(!ready){
-    return (<AppLoading 
-    startAsync={startLoading}
-    onFinish={onFinish} 
-    onError={console.error}/>);  
+  const [assets] = useAssets("https://avatars.githubusercontent.com/u/60842443?s=400&u=9351ff717325e85b9ae172c2001d13c73bb645be&v=4");
+  const [loaded] = Font.useFonts(Ionicons.font);
+  
+  if(!assets || !loaded){
+    return (<AppLoading/>);  
   }
   return <Text>We are done loading!</Text>;
 }
